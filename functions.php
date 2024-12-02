f6c99825e7fc5b2acebe8471ea06c4e4a8d46b3a
Aleksei-chance <aleksei.chance@gmail.com>
2024-12-03 00:28:50 +0300

    addPost/updatePost

--- functions.php
+++ functions.php
@@ -20,5 +20,69 @@ function getPost($id):string
 
     $data = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    return json_encode($data);
+    if($data)
+    {
+        return json_encode($data);
+    }
+    http_response_code(404);
+    $res = [
+        'status' => false,
+        'message' => 'Post not fount'
+    ];
+    return  json_encode($res);
+}
+
+function error($code = 404):string
+{
+    http_response_code($code);
+    $ret = [
+        'status' => false,
+        'massage' => 'Bad request',
+    ];
+    return json_encode($ret);
+}
+
+function addPost($data):string
+{
+    global $db;
+    if(isset($data['title']) && isset($data['body']))
+    {
+        $sql = "INSERT INTO posts (`title`, `body`) VALUES (:title, :body)";
+        $stmt = $db->prepare($sql);
+        $stat = $stmt->execute([
+            'title' => $data['title'],
+            'body' => $data['body'],
+        ]);
+        if($stat){
+            http_response_code(201);
+            $ret = [
+                'status' => true,
+                'post' => $db->lastInsertId()
+            ];
+            return json_encode($ret);
+        }
+    }
+    return error();
+}
+
+function updatePost($id, $data):string
+{
+    global $db;
+    if(isset($data['title']) && isset($data['body'])){
+        $sql = "UPDATE `posts` SET `title` = :title, `body` = :body WHERE id = :id";
+        $stmt = $db->prepare($sql);
+        $stat = $stmt->execute([
+            'id' => $id,
+            'title' => $data['title'],
+            'body' => $data['body'],
+        ]);
+        if($stat){
+            $ret = [
+                'status' => true,
+                'massage' => 'Post is updated'
+            ];
+            return json_encode($ret);
+        }
+    }
+    return error();
 }
--- index.php
+++ index.php
@@ -6,6 +6,7 @@ require_once 'connect.php';
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/functions.php';
 
+$method = $_SERVER['REQUEST_METHOD'];
 
 $q = $_GET['q'] ?? null;
 if(!$q){
@@ -16,12 +17,33 @@ $params = explode('/', $q);
 $type = $params[0];
 $id = $params[1] ?? 0;
 
-if($type === 'posts') {
-    echo getPosts();
+if($method === 'GET'){
+    if($type === 'posts') {
+        echo getPosts();
+    } else if($type === 'post' && $id) {
+        echo getPost($id);
+    } else {
+        echo error();
+    }
+} else if ($method === 'POST') {
+    if($type === 'post') {
+        echo addPost($_POST);
+    } else {
+        echo error();
+    }
+} else if($method === 'PATCH') {
+    if($type === 'post' && $id) {
+        $data = file_get_contents('php://input');
+        $data = json_decode($data, true);
+        echo updatePost($id, $data);
+    } else {
+        echo error();
+    }
+} else {
+    echo error(405);
 }
-else if($type === 'post') {
-    echo getPost($id);
-}
+
+
 
 
 
