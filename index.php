db43341be98afcdcb46d9f5aa45c5cf1b60beb7d
Aleksei-chance <aleksei.chance@gmail.com>
2024-12-03 00:37:27 +0300

    headers

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: *, Authorization');
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Credentials: true');
 header('Content-Type: application/json');
 
 require_once 'connect.php';
